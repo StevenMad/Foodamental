3795b4053893031e1416a06cb3ec6d10ee6b9097
StevenMad <steven-madhow@outlook.fr>
2016-05-02 16:22:36 +0200

    Ajout de l'écran recettes

--- app/src/main/java/com/foodamental/Recipes.java
+++ app/src/main/java/com/foodamental/Recipes.java
@@ -1,8 +1,6 @@
 package com.foodamental;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
@@ -14,61 +12,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.w3c.dom.Text;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class MyMainPage extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class Recipes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_main_page);
+        setContentView(R.layout.activity_recipes);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        //addContentView(textView1,this.getLayoutInflater());
-        TextView textView = (TextView) findViewById(R.id.textView);
-        Intent intent = getIntent();
-
-        //recupération du json à la création
-        try {
-            FileInputStream filein = openFileInput("connexion.json");
-            InputStreamReader reader = new InputStreamReader(filein);
-            char[] inputReadBuffer = new char[1024];
-            String s = "";
-            int charRead;
-
-            while ((charRead=reader.read(inputReadBuffer))>0)
-            {
-                String readString = String.copyValueOf(inputReadBuffer,0,charRead);
-                s+=readString;
-            }
-            reader.close();
-            JSONObject json = new JSONObject(s);
-            String value = json.getString("prenom");
-            textView.setText("Bonjour " + value);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        } catch (JSONException e) {
-            Toast.makeText(MyMainPage.this,"text is not in json format",Toast.LENGTH_LONG).show();
-            e.printStackTrace();
-        }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -76,7 +28,6 @@ public class MyMainPage extends AppCompatActivity
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -92,7 +43,7 @@ public class MyMainPage extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my_main_page, menu);
+        getMenuInflater().inflate(R.menu.recipes, menu);
         return true;
     }
 
@@ -110,33 +61,29 @@ public class MyMainPage extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
-
+    /*
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_frigo) {
-            Intent intent = new Intent(this,Courses.class);
-            startActivity(intent);
-        }else if (id == R.id.nav_recettes) {
-            Intent intent = new Intent(this,Recipes.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_profil) {
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_param) {
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
 
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    public void openCamera(View view)
-    {
-        Intent intent = new Intent(MediaStore.INTENT_ACTION_VIDEO_CAMERA);
-        startActivity(intent);
-    }
+    }*/
 }
