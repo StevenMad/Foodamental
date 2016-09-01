52b2f989fbac03abd174cafb19bbec94efcd7c90
yderbal <YOUSSEF@mbp-de-derbal.home>
2016-09-01 18:55:25 +0200

    gestion couleur produit et format date
    couleur produit et format date

--- app/src/main/java/com/foodamental/activity/Courses.java
+++ app/src/main/java/com/foodamental/activity/Courses.java
@@ -23,8 +23,11 @@ import com.foodamental.util.Tweet;
 import com.foodamental.util.TweetAdapter;
 
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Activité qui gère les produits du frigo
@@ -35,6 +38,8 @@ public class Courses extends AppCompatActivity implements NavigationView.OnNavig
     List<Tweet> tweets;
     TweetAdapter adapter;
     FrigoDB frigo = new FrigoDB();
+    int[] color = {Color.GREEN, Color.YELLOW, Color.RED, Color.BLACK};
+    SimpleDateFormat myFormat = new SimpleDateFormat("dd/MM/yyyy");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,18 +73,19 @@ public class Courses extends AppCompatActivity implements NavigationView.OnNavig
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> a, View v, final int position, long id) {
-                AlertDialog.Builder adb=new AlertDialog.Builder(Courses.this);
+                AlertDialog.Builder adb = new AlertDialog.Builder(Courses.this);
                 adb.setTitle("Delete?");
                 adb.setMessage("Are you sure you want to delete " + position);
                 adb.setNegativeButton("Cancel", null);
                 adb.setPositiveButton("Ok", new AlertDialog.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
-                        Log.d("iddddd",tweets.get(position).getId().toString());
+                        Log.d("iddddd", tweets.get(position).getId().toString());
                         frigo.deleteProductWithId(tweets.get(position).getId());
 
                         adapter.remove(adapter.getItem(position));
                         adapter.notifyDataSetChanged();
-                    }});
+                    }
+                });
                 adb.show();
             }
         });
@@ -87,6 +93,7 @@ public class Courses extends AppCompatActivity implements NavigationView.OnNavig
 
     /**
      * Fonction qui remplit les éléments qui vont remplir la liste view
+     *
      * @return
      * @throws ParseException
      */
@@ -94,8 +101,8 @@ public class Courses extends AppCompatActivity implements NavigationView.OnNavig
         List<Tweet> tweets = new ArrayList<Tweet>();
 
         List<FrigoObject> produit = frigo.getAllProduct();
-        for (FrigoObject prod : produit){
-            tweets.add(new Tweet(Color.BLACK, prod.getName(), prod.getDatePerempt().toString(), prod.getIdFrigo()));
+        for (FrigoObject prod : produit) {
+            tweets.add(new Tweet(getColorByDate(prod.getDatePerempt()), prod.getName(), myFormat.format(prod.getDatePerempt()), prod.getIdFrigo()));
         }
         //tweets.add(new Tweet(Color.BLACK, "Florent", "Mon premier tweet !"));
         //tweets.add(new Tweet(Color.BLUE, "Kevin", "C'est ici que ça se passe !"));
@@ -107,15 +114,29 @@ public class Courses extends AppCompatActivity implements NavigationView.OnNavig
 
     /**
      * Fonction qui gère la navigation du menu
+     *
      * @param item
      * @return
      */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        return MyMenu.onNavigationItemSelected(this,this,item);
+        return MyMenu.onNavigationItemSelected(this, this, item);
     }
 
+    private int getColorByDate(Date date) {
+        Date dateCurrent = new Date();
+        long diff = date.getTime() - dateCurrent.getTime();
+        long result = TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+        if (result >= 30)
+            return color[0];
+        else if (result < 30 && result > 7)
+            return color[1];
+        else if (result < 7 && result > 0)
+            return color[2];
+        else
+            return color[3];
+    }
 
 
 }
