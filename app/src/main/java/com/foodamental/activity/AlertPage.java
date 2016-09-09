9a4325024896349f34c6da08ca12791e5ce64507
yderbal <YOUSSEF@MacBook-Pro-de-DERBAL.local>
2016-09-09 09:16:27 +0200

    comité notif
    commit motif

--- app/src/main/java/com/foodamental/activity/AlertPage.java
+++ app/src/main/java/com/foodamental/activity/AlertPage.java
@@ -1,8 +1,15 @@
 package com.foodamental.activity;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.Menu;
 import android.widget.CompoundButton;
 import android.widget.Switch;
@@ -13,6 +20,7 @@ import com.foodamental.R;
 public class AlertPage extends Activity {
     private Switch mySwitch;
     private TextView switchStatus;
+    private static int notifyId = 2016;
 /*    AlarmManager alarmManager;
     private PendingIntent pendingIntent;
     private TimePicker alarmTimePicker;
@@ -54,6 +62,7 @@ public class AlertPage extends Activity {
                     SharedPreferences.Editor editor = getSharedPreferences("com.mobileapp.smartapplocker", MODE_PRIVATE).edit();
                     editor.putBoolean("service_status", mySwitch.isChecked());
                     editor.commit();
+                    createNotification();
                 } else {
                     switchStatus.setText("Switch is currently OFF");
                     SharedPreferences.Editor editor = getSharedPreferences("com.mobileapp.smartapplocker", MODE_PRIVATE).edit();
@@ -73,6 +82,37 @@ public class AlertPage extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my_main_page, menu);
         return true;
+    }
+
+    public void createNotification() {
+        Intent intent = new Intent(this, Courses.class);
+// use System.currentTimeMillis() to have a unique ID for the pending intent
+        PendingIntent pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+        Intent intent2 = new Intent(this, Recipes.class);
+// use System.currentTimeMillis() to have a unique ID for the pending intent
+        PendingIntent pIntent2 = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent2, 0);
+// build notification
+// the addAction re-use the same intent to keep the example short
+        Notification n  = new Notification.Builder(this)
+                .setContentTitle("Attention vos produits vont périmés!!")
+                .setContentText("Choisir un des deux menus")
+                .setSmallIcon(R.drawable.ic_launcher1)
+                .setContentIntent(pIntent)
+                .setAutoCancel(true)
+                .addAction(R.drawable.common_signin_btn_icon_light, "Frigo", pIntent)
+                .addAction(R.drawable.common_signin_btn_icon_light, "Recettes", pIntent2)
+                .build();
+
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        notificationManager.notify(notifyId, n);
+    }
+    public static void cancelNotification(Context ctx) {
+        String ns = Context.NOTIFICATION_SERVICE;
+        NotificationManager nMgr = (NotificationManager) ctx.getSystemService(ns);
+        nMgr.cancel(notifyId);
     }
 }
 
